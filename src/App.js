9206f42e70c430685954a579537093649e2f7a3a
V Lee <155444512+vir16898@users.noreply.github.com>
2024-03-06 21:32:00 +0800

    Adding semetic tag and metadata

--- src/App.js
+++ src/App.js
@@ -3,9 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Test Little Lemon
-    </div>
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="index.html" role="button">Home</a></li>
+          <li><a href="#.html" role="button">About</a></li>
+          <li><a href="#.html" role="button">Menu</a></li>
+        </ul>
+      </nav>
+      <div class="header-logo">
+        <img src="https://i.postimg.cc/jj4v01tn/Banner.jpg" alt="Lucky Shrub's header logo" />
+      </div>
+    </header>
+
+    <main>
+      <section>
+        <article>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <a href="/book" role="button">Reserve a Table</a>
+        </article>
+      </section>
+    </main>
+
+    <footer>
+    </footer>
+    </>
   );
 }
 
