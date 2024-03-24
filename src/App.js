a4e6928a94f2ea2ab2016a71fd1e5c3c700d5576
V Lee <155444512+vir16898@users.noreply.github.com>
2024-03-24 13:17:49 +0800

    update for deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {BrowserRouter} from 'react-router-dom';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Header/>
         <Footer />
       </BrowserRouter>
