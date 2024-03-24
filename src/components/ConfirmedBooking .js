5b2d8604a901dc3b9cc91be2b142e795da296458
V Lee <155444512+vir16898@users.noreply.github.com>
2024-03-24 13:17:49 +0800

    update for deploy

--- src/components/ConfirmedBooking .js
+++ src/components/ConfirmedBooking .js
@@ -9,12 +9,16 @@ function ConfirmedBooking  (props) {
     return (
         <form>
             <div className="msg-box div-1">
-                <h1>Booking Successfully!</h1>
-                <h3>
-                    Thank you for booking a table in Little Lemon Restaurant!<br/>
-                    We are looking forward to see you!
-                </h3>
-                <Link to="/" className="btn-primary">Back to home</Link>
+                <div>
+                    <h1>Booking Successfully!</h1>
+                    <h3>
+                        Thank you for booking a table in Little Lemon Restaurant!<br/>
+                        We are looking forward to see you!
+                    </h3>
+                </div>
+                <div>
+                    <Link to="/" className="btn-primary">Back to home</Link>
+                </div>
             </div>
         </form>
     )
